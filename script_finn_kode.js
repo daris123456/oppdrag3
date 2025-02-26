d1ca3d95e5425ea126813ccb2f8a70a72d3b69cf
Them2008 <theia005@osloskolen.no>
2025-02-26 22:25:23 +0100

    jeg har endret på farger slik at de blir like som de andre sidene og lakt til mer i javascipt

--- script_finn_kode.js
+++ script_finn_kode.js
@@ -1,1 +1,18 @@
-console.log("endring");
+function sjekkkode() {
+
+    
+document.getElementById("sjekke_boks").addEventListener("click", function()){ 
+    let == number
+    document.parentElement.
+    const kode = 52342; // jeg bruker (const) som betyr at det ikke kan endre svar etter at det er brukt.
+
+    if (kode == 52342); {
+        window.location.href ="";
+
+    } else {
+        document.createElement("div").textContent("denne koden er feil.")
+        document.body.prepend()
+        
+    }
+    
+}
