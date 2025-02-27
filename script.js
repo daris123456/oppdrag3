0a9768a6caf9cf9556db22a53003ee4e5cd9b5ba
daris123456 <daoma015@osloskolen.no>
2025-02-27 12:59:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function myFunction() {
     alert("Game Over");
-    
+    window.location.href = "http://127.0.0.1:5500/losing.html";
+}
+
+function myEscape() {
+    window.location.href = "http://127.0.0.1:5500/winning.html";
+
 }
 
